4d20139eceb933ab81b07924cdfa2c23b17e9a9f
jedevan <jedevancagulada@gmail.com>
2026-08-20 22:20:50 +0800

    Add basic formatting examples in python_basics_formatting.py

---
+++ folder/python_basics_formatting.py
@@ -0,0 +1,18 @@
+bill = 23.60
+#tip is 25% of total bill
+tip = bill * 0.25
+print("The tip is ${:<8.2f}".format(tip))
+
+
+print("hello \nWorld")
+
+
+#print (Hello \f World")
+
+print("Mabuhay \t Philippines!")
+
+print ("{:^100s}".format("Hi"))
+
+if (N==0):
+	print("Is N=0?",N==o)
+	print("Number is Zero")
